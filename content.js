9f29e61fb5633a25a69aa7c829691b853003be50
Felix Thea <felixthea@gmail.com>
2014-02-22 13:56:43 -0500

    capture each word as user types

--- content.js
+++ content.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
 	console.log("in the content.js file");
-	$(document).keyup(function(event){
-		console.log($('div.Am.Al.editable.LW-avf').text());
+	var currentWord = "";
+	var key;
+	var word;
+
+	$(document).keydown(function(event){
+		key = event.keyCode
+		if(key == 32){
+			console.log("'" + currentWord.trim() + "'");
+			currentWord = "";
+		}
+		currentWord += String.fromCharCode(key)
 	})
+
+	// console.log($(event.target).text());	
+
+	// $.ajax({
+	// 	url: "http://words.bighugelabs.com/api/2/7b6ad11fccc077c6e8794f11597d63e9/brave/json",
+	// 	success: function(data, textStatus, jqXHR){
+	// 		console.log(data);
+	// 	},
+	// 	dataType: "json"
+	// })
 })
