f70d413734619fee9ea78654085104a05605106b
Felix Thea <felixthea@gmail.com>
2014-02-22 21:38:49 -0500

    use keypress for better compatibility

--- content.js
+++ content.js
@@ -5,62 +5,63 @@ $(document).ready(function(){
 	var word;
 	var syn;
 	var synList = {
-		"COMEDIC": "humorous1",
-		"FUNNY": "humorous2",
-		"LAUGH": "humorous3",
-		"AMUSING": "humorous4",
-		"ZANY": "humorous5"
+		"comedic": "humorous1",
+		"funny": "humorous2",
+		"laugh": "humorous3",
+		"amusing": "humorous4",
+		"zany": "humorous5"
 	}
 
+	var endCharCodes = [' ', '.', '?', '!'].map(function(character) { 
+		return character.charCodeAt(); 
+	})
+
+	endCharCodes.push(13); //adding in the newline charcode
+
 	function createNoticeModal() {
-		$('body').prepend("<div id='vocab' class='hidden'></div>");
+		$('body').prepend("<div id='vocab'></div>");
 	}
 
 	function showVocabWord(word){
-		console.log("I'm in showVocabWord");
-		$('div#vocab').html('');
-		$('div#vocab').html(word);
-		$('div#vocab').removeClass('hidden');
+		$v = $('div#vocab')
+		$v.empty();
+		$v.fadeIn();
+		$v.html(word);
+		// $v.removeClass('hidden');
 		window.setTimeout(function() {
-      		$('div#vocab').addClass('hidden');
+      		$v.fadeOut();
     	}, 3000)
 	}
 
-	$(document).keydown(function(event){
-		key = event.keyCode
-		letter = String.fromCharCode(key)
+	$(document).keypress(function(event){
+		console.log(event);
+		charCode = event.charCode;
+		letter = String.fromCharCode(charCode);
 
-		if(key == 32){ //space
-			console.log(currentWord.trim());
+		if($.inArray(charCode, endCharCodes) > -1){ 
+			console.log(currentWord);
 
-			syn = checkVocab(currentWord.trim())
-			console.log(syn)
-			if (syn !== undefined){
-				showVocabWord(syn);
-			}
+			syn = getSyn(currentWord.toLowerCase())
+
+			if (syn !== undefined) { showVocabWord(syn); }
+
 			currentWord = "";
-		} else if(key == 8){ //delete, backspace
-			currentWord = currentWord.substr(0,currentWord.length-1)
 		} else {
 			currentWord += letter
 		}
 	})
 
-	function checkVocab(word) {
+	$(document).keydown(function(event){
+		if (event.keyCode == 8) {
+			currentWord = currentWord.substr(0,currentWord.length-1)
+		}
+	})
+
+	function getSyn(word) {
 		if (word in synList) {
 			return synList[word];
 		}
 	}
 
 	createNoticeModal();
-
-	// console.log($(event.target).text());	
-
-	// $.ajax({
-	// 	url: "http://words.bighugelabs.com/api/2/7b6ad11fccc077c6e8794f11597d63e9/brave/json",
-	// 	success: function(data, textStatus, jqXHR){
-	// 		console.log(data);
-	// 	},
-	// 	dataType: "json"
-	// })
 })
