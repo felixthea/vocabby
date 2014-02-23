14f293d1ce177297e3903b3976fe1185149c518f
Felix Thea <felixthea@gmail.com>
2014-02-22 19:13:31 -0500

    show user possible vocab replacement

--- content.js
+++ content.js
@@ -4,27 +4,46 @@ $(document).ready(function(){
 	var key;
 	var word;
 	var syn;
+	var synList = {
+		"COMEDIC": "humorous1",
+		"FUNNY": "humorous2",
+		"LAUGH": "humorous3",
+		"AMUSING": "humorous4",
+		"ZANY": "humorous5"
+	}
+
+	function createNoticeModal() {
+		$('body').prepend("<div id='vocab'></div>");
+		$('body').css('z-index', 1000);
+	}
+
+	function showVocabWord(word){
+		$('div#vocab').html(word);
+	}
 
 	$(document).keydown(function(event){
 		key = event.keyCode
-		if(key == 32){
-			// console.log("'" + currentWord.trim() + "'");
+		if(key == 32){ //space
+			console.log(currentWord.trim());
+
 			syn = checkVocab(currentWord.trim())
 			if (syn !== 'undefined'){
-				console.log("here's a synonym: " + syn)
-			};
+				console.log("here's a synonym: " + syn);
+				showVocabWord(syn);
+			}
 			currentWord = "";
+		} else if(key == 8){ //delete, backspace
+			currentWord = currentWord.substr(0,currentWord.length-1)
+		} else {
+			currentWord += String.fromCharCode(key)
 		}
-		currentWord += String.fromCharCode(key)
 	})
 
 	function checkVocab(word) {
-		if (word == "BRAVE") {
-			return "courageous";
-		} else {
-			return 'undefined';
-		}
+		return synList[word];
 	}
+
+	createNoticeModal();
 
 	// console.log($(event.target).text());	
 
